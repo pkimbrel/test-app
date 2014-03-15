929195baa64696c38d411cd3b00131e30d87c471
pkimbrel <pkimbrel@gmail.com>
2014-03-15 15:43:58 -0500

    Making the app Karma complete

---
+++ spec/test-module.spec.js
@@ -0,0 +1,11 @@
+/**
+ * Created by pkimbrel on 3/15/14.
+ */
+define(['test-module'], function(testModule) {
+	describe("Test Module", function() {
+		it("Should have a message equal to 'Hello!'", function() {
+			//expect(example.message).toBe('Hello!');
+			expect(true).toBe(true);
+		});
+	});
+});
