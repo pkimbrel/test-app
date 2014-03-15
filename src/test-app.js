d746db9abc00de58bebd310f6d3c57ee4b797b00
pkimbrel <pkimbrel@gmail.com>
2014-03-15 13:47:53 -0500

    Updating to add module to the muck and the mire.

--- src/test-app.js
+++ src/test-app.js
@@ -1,10 +1,10 @@
 require.config({
-    baseUrl: "/",
+    baseUrl: "",
     paths: {
-        "jquery": "lib/jquery-1.7.1"
+	    "jquery": "lib/jquery-1.7.1",
+	    "test-module": "src/test-module"
     }
 });
 
 require(['jquery', 'test-module'],function($) {
-
 });
---
+++ src/test-module.js
@@ -0,0 +1,10 @@
+define(['jquery'], function ($) {
+	(function ($) {
+		$.fn.findAgent = function () {
+			return this.each(function() {
+			});
+		};
+
+		$(".find-agent").findAgent();
+	}(jQuery));
+});
